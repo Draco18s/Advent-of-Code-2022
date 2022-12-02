9663c1587b720978c7dc23c0981b898ec6da854b
Draco18s <draco18s@gmail.com>
2022-12-02 02:08:25 -0600

    Day 2 parts 1 and 2

---
+++ Day2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent_of_Code_2021 {
+	internal static class DayTwo {
+		internal static long Part1(string input) {
+			string[] lines = input.Split('\n');
+			int sum = 0;
+			List<(RPS opponent, RPS play)> list = new List<(RPS, RPS)>();
+			foreach(string lin in lines) {
+				if (string.IsNullOrWhiteSpace(lin)) continue;
+				RPS op = (RPS)(lin[0] - 'A' + 1);
+				RPS me = (RPS)(lin[2] - 'X' + 1);
+				list.Add((op, me));
+			}
+			foreach(var g in list)
+			{
+				sum += (int)g.play;
+				sum += ResolveP1(g);
+			}
+			return sum;
+		}
+
+		private static int ResolveP1((RPS opponent, RPS play) g)
+		{
+			if (g.opponent == g.play) return 3;
+			if (g.opponent == g.play+1 || (g.opponent == RPS.ROCK && g.play == RPS.SCISSORS)) return 0;
+			return 6;
+		}
+
+		internal static long Part2(string input) {
+			string[] lines = input.Split('\n');
+			int sum = 0;
+			List<(RPS opponent, RPS play)> list = new List<(RPS, RPS)>();
+			foreach (string lin in lines)
+			{
+				if (string.IsNullOrWhiteSpace(lin)) continue;
+				RPS op = (RPS)(lin[0] - 'A' + 1);
+				RPS me = (RPS)(lin[2] - 'X' + 1);
+				list.Add((op, me));
+			}
+			foreach (var g in list)
+			{
+				sum += GetScore(g.play);
+				sum += ResolveP2(g);
+			}
+			return sum;
+		}
+
+		private static int GetScore(RPS play)
+		{
+			switch(play)
+			{
+				case RPS.ROCK: return 0; //loss
+				case RPS.PAPER: return 3; //tie
+				case RPS.SCISSORS: return 6; //win
+			}
+			return 0;
+		}
+
+		private static int ResolveP2((RPS opponent, RPS play) g)
+		{
+			int v = 0;
+			if (g.play == RPS.PAPER) //end in tie
+			{
+				return (int)g.opponent;
+			}
+			if (g.play == RPS.SCISSORS) //end in win
+			{
+				v = ((int)g.opponent + 1);
+				if (v == 4) v = 1;
+				return v;
+			}
+			v = ((int)g.opponent - 1);
+			if (v == 0) v = 3;
+			return v;
+		}
+
+		private enum RPS
+		{
+			NONE,//0
+			ROCK,//1
+			PAPER,//2
+			SCISSORS,//3
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 C# Solution for Advent of Code 2022
 
 https://adventofcode.com/2022/
@@ -6,3 +7,8 @@ https://adventofcode.com/2022/
       --------Part 1--------   --------Part 2--------
 Day       Time   Rank  Score       Time   Rank  Score
   1   00:03:24  1427      0   00:04:31  1033      0
+  2   02:55:41  31150     0   03:03:09  28442     0
+```
+
+*Day 1: Actually started on time for once.*
+*Day 2: Started late because I misread the clock and got pulled into some Gunfire Reborn for 2 hours. It was worth it. Actual time taken: approximately 00:10:00, 00:08:00*
